f120991c184496588663d3dba9612d6a430727f6
king-of-the-frogs <vlad.osnos@mail.ru>
2022-12-14 00:55:41 +0300

    -Initial commit

---
+++ app/src/main/java/com/example/weatherfetcherapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.weatherfetcher
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewHello = findViewById<TextView>(R.id.tvHello)
+        textViewHello.text = "Hello from code!"
+
+    }
+}
